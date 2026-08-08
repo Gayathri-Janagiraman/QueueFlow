35b327a087675ad1747089b488fd04a97bba05a1
Gayathri-Janagiraman <gayathrijanagiraman86@gmail.com>
2026-08-08 21:32:56 +0530

    Configure production CORS

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import queueRoutes from "./routes/queueRoutes.js";
 import tokenRoutes from "./routes/tokenRoutes.js";
@@ -14,31 +14,46 @@ import { initializeSocket } from "./socket/socket.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+// Express CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
 app.use(express.json());
+
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/queues", queueRoutes);
 app.use("/api/tokens", tokenRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
+// Error Handler
 app.use(errorHandler);
 
+// Root Route
 app.get("/", (req, res) => {
   res.send("QueueFlow API is running...");
 });
 
+// Socket.io
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PATCH"],
   },
 });
+
 initializeSocket(io);
 
+// Server Port
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
